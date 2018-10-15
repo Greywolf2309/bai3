bd20669edebab0ef5b9d8ddb7330fe3983cf417b
Doan Tu <daontu2309@gmail.com>
2018-10-16 01:04:20 +0700

    add

---
+++ test/test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Tu Doan on 16/10/2018.
+//  Copyright © 2018 DoanVantu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello world")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
